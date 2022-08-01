e46af01c5e54ab4214d560cec4ae4869d93e701b
smit <smitd.mobio@gmail.com>
2022-08-01 18:19:24 +0530

    created Item model

--- models.py
+++ models.py
@@ -1,8 +1,13 @@
 from database import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Boolean, Column, DateTime, Integer, String
 
 
-class ToDos(Base):
+class Items(Base):
     __tablename__ = 'todos'
     id = Column(Integer, primary_key = True)
-    task = Column(String(100))
+    name = Column(String(100))
+    price = Column(Integer, nullable = False)
+    on_offer = Column(Boolean, nullable = True)
+    created_on = Column(DateTime, nullable = False)
+    updated_on = Column(DateTime, nullable = True)
+    
