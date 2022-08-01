d61adb0b9e1df241b9c30a8e2e49c31709d91bfb
smit <smitd.mobio@gmail.com>
2022-08-01 18:16:02 +0530

    created api for post method

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from xml.etree.ElementTree import C14NWriterTarget
-from fastapi import FastAPI
-from fastapi_sqlalchemy import DBSessionMiddleware, db
-from config import Config
+from datetime import datetime
+from fastapi import FastAPI, status
+from fastapi_sqlalchemy import db
+import uvicorn
+from database import SessionLocal
+from schema import Items
+import models
+
 app = FastAPI()
 
-app.add_middleware(middleware_class=DBSessionMiddleware, db_url = Config.SQLALCHEMY_DATABASE_URL)
+db = SessionLocal()
+
+
+@app.post('/items', response_model=Items, status_code=status.HTTP_201_CREATED)
+def create_an_item(item:Items):
+    new_item = models.Items(name = item.name, price = item.price, on_offer = item.on_offer, created_on = datetime.now(), updated_on = datetime.now())
+    db.add(new_item)
+    db.commit()
+    return new_item
+
+if __name__ == "__main__":
+    uvicorn.run(app)
